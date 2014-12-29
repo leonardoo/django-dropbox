4caaba34a2e109340f90ac7f705cbd039d741049
leonardo orozco <leonardoorozcop@gmail.com>
2014-12-28 20:57:16 -0500

    settings

--- django_dropbox/runtests/runtests.py
+++ django_dropbox/runtests/runtests.py
@@ -2,12 +2,13 @@
 import os
 import sys
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
+
 import django
 from django.conf import settings
 from django.test.utils import get_runner
 
 if __name__ == "__main__":
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
     if django.VERSION >= (1, 7, 0):
         # starting from 1.7.0 we need to run setup() in order to populate
         # app config
