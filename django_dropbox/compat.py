b4e854ac099f43f15c15d1c3ce62730ba022e6b9
leonardo orozco <leonardoorozcop@gmail.com>
2014-12-28 22:14:33 -0500

    conver indent to spaces

--- django_dropbox/compat.py
+++ django_dropbox/compat.py
@@ -3,14 +3,14 @@ from io import BytesIO, StringIO
 from django.utils import six
 
 try:
-	from django.utils.deconstruct import deconstructible
+    from django.utils.deconstruct import deconstructible
 except ImportError: # Django 1.7+ migrations
-	deconstructible = lambda klass, *args, **kwargs: klass
+    deconstructible = lambda klass, *args, **kwargs: klass
 
 
 def getFile(content):
-	if six.PY3:
-		stream_class = StringIO if isinstance(content, six.text_type) else BytesIO
+    if six.PY3:
+        stream_class = StringIO if isinstance(content, six.text_type) else BytesIO
     else:
         stream_class = BytesIO
         content = force_bytes(content)
