938f90be4491b1640c36633e582380622d125722
leonardo orozco <leonardoorozcop@gmail.com>
2015-03-15 16:07:24 -0500

    force to text file name in model

--- django_dropbox/runtests/dbtest/models.py
+++ django_dropbox/runtests/dbtest/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django_dropbox.storage import DropboxStorage
+from django.utils.encoding import force_text
 
 
 STORAGE = DropboxStorage(location="/test/djangodropbox")
@@ -16,4 +17,4 @@ class TestDropbox(models.Model):
     file_test = models.FileField(upload_to=".",storage = STORAGE, null=True)
 
     def __str__(self):
-        return self.file_test.name
+        return force_text(self.file_test.name)
