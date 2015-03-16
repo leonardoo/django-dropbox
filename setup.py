90cff9b96e209686baa51876374ecda2e03a99ae
leonardo orozco <leonardoorozcop@gmail.com>
2015-03-16 12:44:59 -0500

    change in setup

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 import os
 from django_dropbox import get_version
-from setuptools import setup
+from setuptools import setup, find_packages
 
-def get_packages():
-    # setuptools can't do the job :(
-    packages = []
-    for root, dirnames, filenames in os.walk('django_dropbox'):
-        if '__init__.py' in filenames:
-            packages.append(".".join(os.path.split(root)).strip("."))
-
-    return packages
 
 requires = ['dropbox>=2.0.0', "urllib3>=1.10.1"]
 
@@ -21,7 +13,7 @@ setup(
     author='Andres Torres Marroquin',
     author_email='andres.torres.marroquin@gmail.com',
     url='https://github.com/andres-torres-marroquin/django-dropbox',
-    packages=get_packages(),
+    packages=find_packages(),
     install_requires=requires,
     license='BSD',
 )
